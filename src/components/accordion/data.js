d9721041a10ede1bb3136b329ae69da480d44e28
sumonaung23 <sumonaung575@gmail.com>
2024-04-02 18:10:42 +0700

    modified folders and files and then added data input into each newly created files

---
+++ src/components/accordion/data.js
@@ -0,0 +1,24 @@
+const quizData = [
+    {
+        id: 1,
+        question: "What is the capital of France?",
+        answer: "Paris",
+        },
+        {
+        id: 2,
+        question: "What is the tallest mountain in the world?",
+        answer: "Mount Everest",
+        },
+        {
+        id: 3,
+        question: "What is the largest ocean on Earth?",
+        answer: "Pacific Ocean",
+        },
+        {
+        id: 4,
+        question: "What is the language spoken in Brazil?",
+        answer: "Portuguese",
+        },
+    ];
+
+export default qui
